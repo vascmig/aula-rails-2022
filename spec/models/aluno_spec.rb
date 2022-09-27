41749dca13bf0422fd79464ef5b681cb0515cdbd
Miguel Vasconcelos <miguel.vasconcelos@usp.br>
2022-09-27 21:37:07 +0000

    cenario feliz e tristes completos

---
+++ spec/models/aluno_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Aluno, type: :model do
+  
+  it 'invalido sem nome' do
+    aluno = Aluno.new
+    aluno.email = "teste@gmail.com"
+    expect(aluno).not_to be_valid
+  end
+  
+  it 'invalido sem email' do
+    a = Aluno.new
+    a.nome = "teste@gmail.com"
+    expect(a).not_to be_valid
+  end
+
+  it 'invalido com email com formato invalido' do
+    a = Aluno.new
+    a.nome= "joao" 
+    a.email = "a,@gmail.com"    
+    expect(a).not_to be_valid
+  end
+end
